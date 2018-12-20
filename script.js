746d6ff8e9a448542cf55ddc834f7270d3c13c61
Agustus <adhithya220@gmail.com>
2018-12-21 01:21:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function redirect(){
+		location.href="https://docs.google.com/forms/d/e/1FAIpQLSdr2EL1xmjuIevPaXRokVdwbqLGY_p5C8JaVSTPLljcG3aQTA/viewform?usp=sf_link"
+	}
+	function flipm(){
+		document.getElementById("turnedm").style.display="none";
+		document.getElementById("flippedm").style.display="block";
+
+	}
+	function turnm(){
+		document.getElementById("turnedm").style.display="block";
+		document.getElementById("flippedm").style.display="none";
+
+	}
+	function flipw(){
+		document.getElementById("turnedw").style.display="none";
+		document.getElementById("flippedw").style.display="block";
+
+	}
+	function turnw(){
+		document.getElementById("turnedw").style.display="block";
+		document.getElementById("flippedw").style.display="none";
+
+	}
